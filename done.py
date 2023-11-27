9412dbd93568af938f3d87c5f0580eb40e539df5
githab11 <147356562+githab11@users.noreply.github.com>
2023-11-27 15:05:45 -0500

    Update done.py

--- done.py
+++ done.py
@@ -36,7 +36,9 @@ HealingPotionCost = 7
 HealingPotionHealth = 20
 
 from colorama import Fore, Style
-
+min_damage_changer = 2
+max_damage_changer = 2
+player_health_changer = 10
 
 
 # Player stats
@@ -70,8 +72,8 @@ if class_input[0].lower() == 'c':
     HealingPotionHealth = 20
 elif class_input[0].lower() == 'g':
     print(Fore.LIGHTWHITE_EX + "You have selected the Glass Cannon")
-    crit_chance = 12
-    dodge_chance = 8
+    crit_chance = 15
+    dodge_chance = 5
     player_health = 35
     min_sword_dmg = 14
     max_sword_dmg = 15
@@ -85,8 +87,8 @@ elif class_input[0].lower() == 'g':
     HealingPotionHealth = 20
 elif class_input[0].lower() == 't':
     print(Fore.BLUE + "You have selected Tank")
-    crit_chance = 8
-    dodge_chance = 12
+    crit_chance = 5
+    dodge_chance = 15
     player_health = 50
     min_sword_dmg = 8
     max_sword_dmg = 12
@@ -98,10 +100,10 @@ elif class_input[0].lower() == 't':
     HealingPotionAmount = 0
     HealingPotionCost = 7
     HealingPotionHealth = 20
-elif class_input.lower() == 'game dev':
+elif class_input[0].lower() == ',':
     print(Fore.RED + "Welcome Jack!")
-    crit_chance = 10
-    dodge_chance = 10
+    crit_chance = 100
+    dodge_chance = 100
     player_health = 10000
     min_sword_dmg = 10000
     max_sword_dmg = 10000
@@ -187,6 +189,7 @@ def set_bandit():
     enemy_attack_min = 5
     enemy_attack_max = 10
     exp = 25
+
 
 
 # Skeleton!
@@ -297,15 +300,15 @@ def gain_xp(amount):
 
         valid_input = False
         while not valid_input:
-            level_up_input = input(Fore.RED + "Upgrade sword damage (2) or health (10)? : ")
+            level_up_input = input(Fore.RED + f"Upgrade sword damage ({min_damage_changer}) or health ({player_health_changer})? : ")
 
             if level_up_input.lower().startswith('s'):
-                min_sword_dmg += 2
-                max_sword_dmg += 2
+                min_sword_dmg += min_damage_changer
+                max_sword_dmg += max_damage_changer
                 print(Fore.RED + f"You have: {min_sword_dmg}-{max_sword_dmg} Damage!")
                 valid_input = True
             elif level_up_input.lower().startswith('h'):
-                player_health += 10
+                player_health += player_health_changer
                 player_maxhealth = player_health
                 print(Fore.RED + f"You have: {player_health} Health!")
                 valid_input = True
@@ -333,19 +336,21 @@ def fight():
         enemy_attack = input(Fore.LIGHTGREEN_EX + f"Would you like to attack the {enemy_name}?: ")
 
         if enemy_attack.lower().startswith('y'):
-            dodge_chance_player = random.randint(1, dodge_chance)
-            crit_chance_player = random.randint(1, crit_chance)
+            enemy_dodge = random.randint(1, 10)
+            enemy_crit = random.randint(1, 10)
 
             plr_actual_dmg = random.randint(min_sword_dmg, max_sword_dmg)
 
             # Check player's dodge chance
-            if dodge_chance_player == dodge_chance:
+            if enemy_dodge == 1:
                 print(Fore.BLACK + f"The {enemy_name} has dodged!")
             else:
                 # Check player's crit chance
-                if crit_chance_player == crit_chance:
+                if random.randint(1, 100) <= crit_chance:
                     plr_actual_dmg *= 1.5
-                    print(Fore.BLUE + f"You've attacked {enemy_name} for: {plr_actual_dmg} With a CRIT!")
+                    print(Fore.MAGENTA + f"You've attacked {enemy_name} for: {plr_actual_dmg} With a CRIT!")
+                elif random.randint(1, 100) <= dodge_chance:
+                    print(Fore.BLUE +   f"You have Dodged!")
                 else:
                     print(Fore.RED + f"You've attacked {enemy_name} for: {plr_actual_dmg}")
 
@@ -365,7 +370,6 @@ def fight():
                 # Check for achievements when defeating an enemy
                 if not achievements['First Victory']:
                     unlock_achievement('First Victory')
-
                 if enemy_name == 'Goblin':
                     unlock_achievement('Goblin Slayer')
                 elif enemy_name == 'Bandit':
@@ -392,16 +396,16 @@ def fight():
             if HealingPotionAmount > 0:
                 print("You have chosen to heal!")
                 print(f"You have healed for: {HealingPotionHealth}")
-                player_health += HealingPotionAmount
+                player_health += HealingPotionHealth
                 HealingPotionAmount -= 1
                 print(f"You now have {HealingPotionAmount} Healing Potions.")
 
         # Update player's health
-        if dodge_chance_player != dodge_chance:
+        if random.randint(1, 100) > dodge_chance:
             actual_dmg = random.randint(enemy_attack_min, enemy_attack_max)
 
             # Check player's dodge chance
-            if dodge_chance_player == dodge_chance - 1:
+            if random.randint(1, 100) <= dodge_chance:
                 print(Fore.BLACK + f"{player_name} has dodged!")
             else:
                 print(Fore.LIGHTRED_EX + f"The {enemy_name} has attacked you for: {actual_dmg}")
@@ -411,6 +415,8 @@ def fight():
 
         print(Fore.RED + f"The {enemy_name} has {enemy_health} health left!")
         print(Fore.LIGHTRED_EX + f"You have {player_health} health left!")
+
+
 
 
 # Game loop, had to rewrite this
@@ -450,16 +456,24 @@ while player_health > 0:
                 openshop()
     elif advance == 2:
 
+
+
         fireorregular = random.randint(1, 4)
         if fireorregular != 4:
+            set_dragon
+            fight()
+            fireorregular = random.randint(1, 4)
+            min_damage_changer = 3
+            max_damage_changer = 3
+            player_health_changer = 20
+
+        elif fireorregular == 4:
             set_fire_dragon()
             fight()
             fireorregular = random.randint(1, 4)
-
-        elif fireorregular == 4:
-            set_dragon()
-            fight()
-            fireorregular = random.randint(1, 4)
+            min_damage_changer = 3
+            max_damage_changer = 3
+            player_health_changer = 20
 
 # Player has died, end the game
 if player_health <= 0:
